ccb2d4b523c2f1ef3cde7344541645d319432bc8
nikosgpet <n.peteinatos@di.uoa.gr>
2017-02-05 19:20:17 +0100

    Add css file

--- workflowy.js
+++ workflowy.js
@@ -1,3 +1,14 @@
+function addCssFile(file) {
+    var head  = document.getElementsByTagName('head')[0];
+    var link  = document.createElement('link');
+    link.rel  = 'stylesheet';
+    link.type = 'text/css';
+    link.href = file;
+    link.media = 'all';
+    head.appendChild(link);   
+}
+addCssFile('https://raw.githubusercontent.com/nikosgpet/tampermonkey/master/tags.workflowy.css');
+
 function addGlobalStyle(css) {
     var head, style;
     head = document.getElementsByTagName('head')[0];
