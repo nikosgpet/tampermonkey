150bbb7fd4ed02339c784cbbcdcbbd0449d57c47
nikosgpet <n.peteinatos@di.uoa.gr>
2017-02-05 18:21:08 +0100

    Update to lighter version

--- workflowy.js
+++ workflowy.js
@@ -1,38 +1,15 @@
-// ==UserScript==
-// @name         New ES6-Userscript
-// @namespace    http://tampermonkey.net/
-// @version      0.2
-// @description  shows how to use babel compiler
-// @author       You
-// @require      https://cdnjs.cloudflare.com/ajax/libs/babel-standalone/6.18.2/babel.js
-// @require      https://cdnjs.cloudflare.com/ajax/libs/babel-polyfill/6.16.0/polyfill.js
-// @match        http://workflowy.com/*
-// ==/UserScript==
+function addGlobalStyle(css) {
+    var head, style;
+    head = document.getElementsByTagName('head')[0];
+    if (!head) { return; }
+    style = document.createElement('style');
+    style.type = 'text/css';
+    style.innerHTML = css;
+    head.appendChild(style);
+}
 
-/* jshint ignore:start */
-var inline_src = (<><![CDATA[
-/* jshint ignore:end */
-    /* jshint esnext: false */
-    /* jshint esversion: 6 */
-
-    function addGlobalStyle(css) {
-        var head, style;
-        head = document.getElementsByTagName('head')[0];
-        if (!head) { return; }
-        style = document.createElement('style');
-        style.type = 'text/css';
-        style.innerHTML = css;
-        head.appendChild(style);
+addGlobalStyle(`
+    #pageContainer {
+        background-color: green;
     }
-    
-    addGlobalStyle(`
-        #pageContainer {
-            background-color: beige;
-        }
-    `);
-/* jshint ignore:start */
-]]></>).toString();
-var c = Babel.transform(inline_src, { presets: [ "es2015", "es2016" ] });
-eval(c.code);
-/* jshint ignore:end */
-
+`);
