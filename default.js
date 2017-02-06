b132203ecc1d757a994e070e4fcd43bcb8459946
nikosgpet <n.peteinatos@di.uoa.gr>
2017-02-06 15:31:23 +0100

    Make compatible with tampermonkey

--- default.js
+++ default.js
@@ -1,32 +1,49 @@
-window.onload = function(){
+// ==UserScript==
+// @name         Workflowy scripts
+// @namespace    http://tampermonkey.net/
+// @version      0.2
+// @description  try to take over the world!
+// @author       You
+// @require      https://raw.githubusercontent.com/nikosgpet/tampermonkey/master/workflowy.js
+// @match        https://*/*
+// @grant        none
+// ==/UserScript==
 
-//alert('Button added');
-function escapeHtml(text) {
-  var map = {
-    '&': '&amp;',
-    '<': '&lt;',
-    '>': '&gt;',
-    '"': '&quot;',
-    "'": '&#039;'
-  };
+(function() {
+    'use strict';
 
-  return text.replace(/[&<>"']/g, function(m) { return map[m]; });
-}
+    window.onload = function(){
 
-var url = location.protocol + '//' + location.host + location.pathname
-var text = '<opml><body><outline text=\'#ref ' + escapeHtml(document.title) + '\' _note=\'' + escapeHtml(location.href) + ' \'/></body></opml>';
+    //alert('Button added');
+    function escapeHtml(text) {
+      var map = {
+        '&': '&amp;',
+        '<': '&lt;',
+        '>': '&gt;',
+        '"': '&quot;',
+        "'": '&#039;'
+      };
 
-var r='<input id="nikos-select" value="' + text + '"/><div id="nikos-button" class="peristeri nikos-button nikos-left"> Copy </div>';
-//$("body").append(r);
-document.body.insertAdjacentHTML('beforeend', r)
-//$('#nikos-select').val(text);
-document.getElementById("nikos-select").value = text;
+      return text.replace(/[&<>"']/g, function(m) { return map[m]; });
+    }
 
-let button = document.getElementById('nikos-button');
+    var url = location.protocol + '//' + location.host + location.pathname
+    var text = '<opml><body><outline text=\'#ref ' + escapeHtml(document.title) + '\' _note=\'' + escapeHtml(location.href) + ' \'/></body></opml>';
 
-button.addEventListener('click', function(e) {
-  e.preventDefault();
-  document.execCommand('copy', false, document.getElementById('nikos-select').select());
-});
+    var r='<input id="nikos-select" value="' + text + '"/><div id="nikos-button" class="peristeri nikos-button nikos-left"> Copy </div>';
+    //$("body").append(r);
+    document.body.insertAdjacentHTML('beforeend', r)
+    //$('#nikos-select').val(text);
+    document.getElementById("nikos-select").value = text;
 
-};
+    let button = document.getElementById('nikos-button');
+
+    button.addEventListener('click', function(e) {
+      e.preventDefault();
+      document.execCommand('copy', false, document.getElementById('nikos-select').select());
+    });
+
+    };
+
+})();
+
