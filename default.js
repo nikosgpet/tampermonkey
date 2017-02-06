5a1707aabc565d34f5333e090642b2b03dcac6ca
nikosgpet <nikosgpet@gmail.com>
2017-02-06 15:23:52 +0100

    Add default.js

---
+++ default.js
@@ -0,0 +1,32 @@
+window.onload = function(){
+
+//alert('Button added');
+function escapeHtml(text) {
+  var map = {
+    '&': '&amp;',
+    '<': '&lt;',
+    '>': '&gt;',
+    '"': '&quot;',
+    "'": '&#039;'
+  };
+
+  return text.replace(/[&<>"']/g, function(m) { return map[m]; });
+}
+
+var url = location.protocol + '//' + location.host + location.pathname
+var text = '<opml><body><outline text=\'#ref ' + escapeHtml(document.title) + '\' _note=\'' + escapeHtml(location.href) + ' \'/></body></opml>';
+
+var r='<input id="nikos-select" value="' + text + '"/><div id="nikos-button" class="peristeri nikos-button nikos-left"> Copy </div>';
+//$("body").append(r);
+document.body.insertAdjacentHTML('beforeend', r)
+//$('#nikos-select').val(text);
+document.getElementById("nikos-select").value = text;
+
+let button = document.getElementById('nikos-button');
+
+button.addEventListener('click', function(e) {
+  e.preventDefault();
+  document.execCommand('copy', false, document.getElementById('nikos-select').select());
+});
+
+};
