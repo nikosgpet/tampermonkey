813e6b8328f012bc32578a863687f5b90aeb43b3
nikosgpet <n.peteinatos@di.uoa.gr>
2017-07-08 21:00:08 +0200

    Add download buttons

--- workflowy.js
+++ workflowy.js
@@ -1,11 +1,11 @@
 // ==UserScript==
 // @name         Workflowy scripts
 // @namespace    http://tampermonkey.net/
-// @version      0.4
+// @version      0.5
 // @description  try to take over the world!
 // @author       You
 // @match        https://workflowy.com/*
-// @grant        none
+// @grant        GM_openInTab
 // ==/UserScript==
 
 (function() {
@@ -19,7 +19,7 @@
         link.href = file;
         link.id   = id;
         link.media = 'all';
-        head.appendChild(link);   
+        head.appendChild(link);
     }
     addCssFile('https://rawgit.com/nikosgpet/tampermonkey/master/tags.workflowy.css', 'tags_css');
     addCssFile('https://rawgit.com/nikosgpet/tampermonkey/master/layout.workflowy.css', 'layout_css');
@@ -35,6 +35,90 @@
         head.appendChild(style);
     }
 
+    function getId() {
+        var url = location.href.split('#/');
+        return (url.length > 1) ? url[1] : '';
+    }
+
+    function addCopyButton() {
+        var r='<div id="nikos-button" class="aidoni nikos-button nikos-left">Quick</div>';
+        document.body.insertAdjacentHTML('beforeend', r);
+        let button = document.getElementById('nikos-button');
+
+        button.addEventListener('click', function(e) {
+            e.preventDefault();
+            GM_openInTab('http://127.0.0.1:4444/workflowy/visualise/?id=' + getId());
+            console.log(url);
+        });
+
+        var r2='<div id="nikos-button2" class="aidoni nikos-button nikos-right">Download</div>';
+        document.body.insertAdjacentHTML('beforeend', r2);
+        let button2 = document.getElementById('nikos-button2');
+
+        button2.addEventListener('click', function(e) {
+            e.preventDefault();
+            GM_openInTab('http://127.0.0.1:4444/workflowy/download/?id=' + getId());
+            console.log(url);
+        });
+    }
+    addCopyButton();
+
+
+    addGlobalStyle(`
+        .nikos-button {
+            display: inline-block;
+            /*   position: relative; */
+            width: 60px;
+            height: 32px;
+            line-height: 32px;
+            border-radius: 2px;
+            font-size: 0.9em;
+            background-color: #eee;
+            color: #646464;
+            transition: box-shadow 0.2s cubic-bezier(0.4, 0, 0.2, 1);
+            transition-delay: 0.2s;
+            box-shadow: 0 2px 5px 0 rgba(0, 0, 0, 0.26);
+
+            text-align: center;
+
+            cursor: default;
+            opacity: 0.5;
+            z-index: 1000;
+        }
+
+        .nikos-left {
+            position: fixed;
+            bottom: 20px;
+            left: 20px;
+        }
+
+        .nikos-right {
+            position: fixed;
+            bottom: 20px;
+            right: 20px;
+        }
+
+        .nikos-success {
+            z-index: 1000;
+            font-size: 2em;
+            opacity: 0.5;
+            left: 100px;
+        }
+
+        .nikos-button:active, .nikos-button:hover {
+            opacity: 1;
+        }
+
+        .nikos-button:active {
+            box-shadow: 0 8px 17px 0 rgba(0, 0, 0, 0.2);
+            transition-delay: 0s;
+        }
+
+        #nikos-select {
+            position: fixed;
+            left: -1000px;
+        }
+    `);
 })();
 
 
