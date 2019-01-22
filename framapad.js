15057b4febf5f79f2e9b4e9d563e9be1a124d1a9
nikosgpet <n.peteinatos@di.uoa.gr>
2019-01-22 14:47:06 +0100

    Update framapad.js

--- framapad.js
+++ framapad.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Framapad scripts
 // @namespace    http://mypads.framapad.org/
-// @version      0.2
+// @version      0.3
 // @description  try to take over the world!
 // @author       You
 // @match        https://mypads.framapad.org/*
