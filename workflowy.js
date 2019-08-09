7ac443ac3e1bdf4fccaaeb23e59339a085e77cb7
nikosgpet <n.peteinatos@di.uoa.gr>
2019-08-09 21:00:58 +0300

    bump version

--- workflowy.js
+++ workflowy.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Workflowy scripts
 // @namespace    http://tampermonkey.net/
-// @version      0.5
+// @version      0.6
 // @description  try to take over the world!
 // @author       You
 // @match        https://workflowy.com/*
